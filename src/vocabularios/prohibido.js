ba37733e7cd9b3f053513b42e3463b7a7b214717
VegaMedHub <raymundo.vega.bonilla@gmail.com>
2025-10-20 06:57:59 -0400

    prohibido.js

---
+++ src/vocabularios/prohibido.js
@@ -0,0 +1,7 @@
+// src/vocabularies/prohibidos.js
+export const TERMINOS_PROHIBIDOS = [
+  "sentir", "opinar", "creer", "inversor", "dinero",
+  "carro", "receta", "chef", "psicología", "emocional",
+  "triste", "feliz", "ansiedad", "depresión", "miedo",
+  "amor", "odio", "estrés", "motivación", "consejo"
+];
